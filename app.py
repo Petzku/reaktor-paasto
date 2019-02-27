6fae0d426580d8d59c67de5f2b6d57a2837688dd
petzku <petzku@zku.fi>
2019-02-28 01:21:38 +0200

    Add feature for updating data automatically if old

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, jsonify, request
 import database
+import handle_csv
+import data_fetch
+import datetime
+
 
 def create_app():
     app = Flask(__name__)
@@ -40,3 +44,19 @@ def create_app():
         return jsonify(response_data)
 
     return app
+
+def update_data():
+    """ checks to see if data needs updating, and update if so
+    currently updates daily, could maybe be less frequent """
+
+    last_update_str, = database.get_database_updated()  # returns a 1-tuple, unpack it
+    last_update = datetime.date.fromisoformat(last_update_str)
+    if last_update < datetime.date.today():
+        popfile, co2file, countryfile = data_fetch.get_dataset_files()
+
+        new_data = load_data(popfile, co2file, countryfile)
+        popfile.close()
+        co2file.close()
+        countryfile.close()
+
+        database.update_database(new_data)
